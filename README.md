93350cffb0df57259efe80c67b8e72054d12d5e3
ramidebs97 <rami.al.debs97@gmail.com>
2020-12-28 11:33:57 +0200

    info added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # washmen-api-task
+when you run the project
+please try to call localhost:3000/companies?km=100 on postman or webpage
+feel free to replace the port (3000)
--- app.js
+++ app.js
@@ -9,6 +9,6 @@ const comapniesRouter = require("./routes/route");
 app.use('/', comapniesRouter);
 
 app.listen(port, () => {
-    console.log("Runing on port " + port);
+    console.log("Try to call localhost:3000/companies?km=100");
 
 });
